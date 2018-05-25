f5197dc1a3bdfba8dfd0d3b313e85afc6b16aaf6
abhishek18620 <abhishek18620@gmail.com>
2018-05-25 21:24:22 +0530

    will be starting ASAP

---
+++ __init__.py
@@ -0,0 +1,19 @@
+"""
+*  Author : abhishek18620
+*  Created On : 2018-05-25
+*  File : __init__.py
+"""
+
+def ii():
+    return int(input())
+
+def mi():
+    return map(int, input().split())
+
+def li():
+    return list(mi())
+
+def solve():
+
+if __name__=="__main__":
+    solve()
